6f6b9b003acf8652657c3ef7a385f9c0f62574ec
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-04-27 13:15:01 +0100

    Add project setup

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import Header from "./src/components/Header";
+import Search from "./src/components/Search";
+import CategoryItem from "./src/components/CategoryItem";
 
 export default function App() {
+  const [term, setTerm] = useState("Burger");
+
+  const commonCatagories = [
+    {
+      name: "Burger",
+      imageUrl: require("./src/assets/images/burger.png"),
+    },
+    {
+      name: "Pizza",
+      imageUrl: require("./src/assets/images/pizza.png"),
+    },
+    {
+      name: "Dessert",
+      imageUrl: require("./src/assets/images/cake.png"),
+    },
+    {
+      name: "Drinks",
+      imageUrl: require("./src/assets/images/smoothies.png"),
+    },
+    {
+      name: "Steak",
+      imageUrl: require("./src/assets/images/steak.png"),
+    },
+    {
+      name: "Pasta",
+      imageUrl: require("./src/assets/images/pasta.png"),
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Search setTerm={setTerm} />
+      <View>
+        <FlatList
+          data={commonCatagories}
+          renderItem={({ item, index }) => {
+            return (
+              <CategoryItem
+                name={item.name}
+                imageUrl={item.imageUrl}
+                index={index}
+                active={item.name === term}
+                handlePress={() => setTerm(item.name)}
+              />
+            );
+          }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(category) => category.name}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +65,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ src/components/CategoryItem.js
@@ -0,0 +1,60 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { elevation } from "../common/styles";
+
+export default function CategoryItem({
+  name,
+  imageUrl,
+  index,
+  active,
+  handlePress,
+}) {
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View
+        style={[
+          styles.container,
+          styles.elevation,
+          index === 0 ? { marginLeft: 25 } : { marginLeft: 15 },
+          active
+            ? { backgroundColor: "rgb(241,186,87)" }
+            : { backgroundColor: "white" },
+        ]}
+      >
+        <View style={styles.imageContainer}>
+          <Image source={imageUrl} style={styles.image} />
+        </View>
+        <Text style={styles.header}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 70,
+    height: 100,
+    borderRadius: 50,
+    marginVertical: 15,
+    backgroundColor: "rgb(241,186,87)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  elevation,
+  image: {
+    width: 35,
+    height: 35,
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    marginBottom: 5,
+  },
+  header: {
+    fontWeight: "bold",
+  },
+});
